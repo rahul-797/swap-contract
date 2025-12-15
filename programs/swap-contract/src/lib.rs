edfcd4e79566a15ee8654f25c71cead90cc98973
rahul-797 <rahul.sangwan797@gmail.com>
2025-12-15 15:14:19 +0530

    swap working

--- programs/swap-contract/src/lib.rs
+++ programs/swap-contract/src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(unexpected_cfgs)]
+#![allow(clippy::result_large_err)]
+
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -15,7 +18,18 @@ declare_id!("8YFUVmE3uWVdqJb1wDUu2qy6e8jmgMZKiRaG9kknaDhE");
 pub mod swap_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+        instructions::take_offer::withdraw_and_close_vault(context)
     }
 }
