e20f0915af8f76d5480e31448de4b46ee26d52da
rahul-797 <rahul.sangwan797@gmail.com>
2025-12-15 20:55:01 +0530

    more readable code using re-exports

--- programs/swap-contract/src/lib.rs
+++ programs/swap-contract/src/lib.rs
@@ -24,12 +24,12 @@ pub mod swap_contract {
         token_a_offered_amount: u64,
         token_b_wanted_amount: u64,
     ) -> Result<()> {
-        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
-        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
+        instructions::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::save_offer(context, id, token_b_wanted_amount)
     }
 
     pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
-        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
-        instructions::take_offer::withdraw_and_close_vault(context)
+        instructions::send_wanted_tokens_to_maker(&context)?;
+        instructions::withdraw_and_close_vault(context)
     }
 }
